6e14ad83ba061bbc3a772085e44aa269f70bd4f0
Dipu George <dipugeorge2505@gmail.com>
2021-09-16 21:37:43 +0530

    my latest

---
+++ test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+print "(Html)"  
+<head>   
+<title>  
+Registration Page  
+</title>  
+</head>  
+<body bgcolor=\"Lightskyblue\">  
+<br>  
+<br>  
+<form>  
+  
+<label> Firstname </label>         
+<input type=\"text\" name=\"firstname\" size=\"15\"/> <br> <br>   
+<label> Lastname: </label>         
+<input type=\"text\" name=\"lastname\" size=\"15\"/> <br> <br>   
+<br> <br>  
+Email:  
+<input type=\"email\" id=\"email\" name=\"email\"/> <br>    
+<br> <br>  
+<form action=\"upload.php\" method=\"post\" en
